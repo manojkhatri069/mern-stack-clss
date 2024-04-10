4d930b70f06b8d1ca3fbe5be3391651ba987a258
Manoj <manojkhatri2061@gmail.com>
2024-04-10 12:28:20 +0545

    manoj

---
+++ day3/script.js
@@ -0,0 +1,30 @@
+function laptop(x){
+    console.log( ` first laptop:the value of x is ${x} and y is ${y}`);
+}
+
+function laptop(x,y,z){
+
+console.log(` second laptop :the value of x is ${x} y is ${y} and z is ${z}`);
+}
+
+laptop(30,40,40,30);
+
+-onst student= {
+    Name:"manoj khatri",
+    Age:19,
+    Subject:"BIT",
+    Semester:"third",
+    Height:5.8,
+    Address:"Bharatpur 11"
+};
+
+console.log(student.Address);
+
+
+console.log("I LOve Javascript")
+
+
+
+
+
+
---
+++ day4/script.js
@@ -0,0 +1,5 @@
+const num =[1,2,3,4];
+console.log(num.length);
+console.log( num[2]);
+console.log(num.sort());
+console.log(num.reverse());
