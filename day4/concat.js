d2ab901f3c12034df6896c990f35b3133ba9ad00
Manoj <manojkhatri2061@gmail.com>
2024-04-10 13:55:10 +0545

    day4

---
+++ day4/concat.js
@@ -0,0 +1,6 @@
+const vegitable =[ "a","b","c","d"];
+const fruit =["banana", "Orange" , "Apple" ,"mango"]
+console.log( fruit.concat(vegitable));
+const firstname="Manoj";
+const lastname="Khatri";
+console.log(firstname,lastname);
